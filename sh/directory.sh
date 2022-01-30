e10af4d389c3db374067a5bf956ffe5a87a6ba31
X.A.N.A <git@vnghia.com>
2022-01-30 18:57:48 +0100

    sh(config): add config and $PATH

---
+++ sh/directory.sh
@@ -0,0 +1,22 @@
+# XDG
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-$HOME/.xdg}"
+
+export XDG_DESKTOP_DIR="${XDG_DESKTOP_DIR:-$HOME/Desktop}"
+export XDG_DOCUMENTS_DIR="${XDG_DOCUMENTS_DIR:-$HOME/Documents}"
+export XDG_DOWNLOAD_DIR="${XDG_DOWNLOAD_DIR:-$HOME/Downloads}"
+export XDG_MUSIC_DIR="${XDG_MUSIC_DIR:-$HOME/Music}"
+export XDG_PICTURES_DIR="${XDG_PICTURES_DIR:-$HOME/Pictures}"
+export XDG_VIDEOS_DIR="${XDG_VIDEOS_DIR:-$HOME/Videos}"
+export XDG_PROJECTS_DIR="${XDG_PROJECTS_DIR:-$HOME/Projects}"
+
+# Dotfiles
+export DUMPFILE_HOME="$DOTFILES_HOME/.dumpfile"
+export HISTFILE_HOME="$DOTFILES_HOME/.histfile"
+
+# Code
+export CODE_HOME="${CODE_HOME:-$HOME/code}"
+export CODE_LOCAL="$CODE_HOME/.local"
+export CODE_BIN="$CODE_LOCAL/global_bin"
