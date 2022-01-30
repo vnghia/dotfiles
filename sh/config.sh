b48d4da2da4c803bcae1a5b89c67f4e925f3c53c
Vo Van Nghia <vanvnghia@gmail.com>
2022-01-30 03:11:04 +0100

    git(config): add config

--- sh/config.sh
+++ sh/config.sh
@@ -12,5 +12,8 @@ export LC_ALL="en_US.UTF-8"
 # Python
 export PIP_REQUIRE_VIRTUALENV="true"
 
+# Git
+export GIT_CONFIG_GLOBAL="$DOTFILES_GIT_HOME/.gitconfig"
+
 # Local configuration / path / everything else that is specific to each machine
 LOCAL_SH="$DOTFILES_SHELL_HOME/.local.sh" && test -f $LOCAL_SH && source $LOCAL_SH
