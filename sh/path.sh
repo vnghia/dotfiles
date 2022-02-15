06c49b963e328dbb4dc6d2665d927d19fa67f07e
Vo Van Nghia <vanvnghia@gmail.com>
2022-02-15 09:09:36 +0100

    sh(script): add script folder

--- sh/path.sh
+++ sh/path.sh
@@ -7,5 +7,8 @@ export PATH="$PYVENV_BIN:$PATH"
 # Python script
 export PATH="$DOTFILES_PYTHON_SCRIPT:$PATH"
 
+# Shell script
+export PATH="$DOTFILES_SHELL_SCRIPT:$PATH"
+
 # Standalone binary
 export PATH="$CODE_BIN:$PATH"
